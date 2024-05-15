92710313ea76390aa7cf5e83a08443cac4f8f95d
Cojiță Filip-Iosia <96796816+filipcojita@users.noreply.github.com>
2024-05-15 15:44:21 +0300

    fisiere updated
    new content for
    index.html, script.js, script2.js, styles.css

--- script.js
+++ script.js
@@ -43,3 +43,21 @@ document.getElementById('contactForm').addEventListener('submit', function(event
         event.preventDefault();
     }
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('theme-toggle');
+    const body = document.body;
+
+    // Check for saved user preference, if any, on load of the website
+    const currentTheme = localStorage.getItem('theme') || 'light';
+    if (currentTheme === 'dark') {
+        body.classList.add('dark-mode');
+    }
+
+    // Add event listener to the toggle button
+    toggleButton.addEventListener('click', () => {
+        body.classList.toggle('dark-mode');
+        const theme = body.classList.contains('dark-mode') ? 'dark' : 'light';
+        localStorage.setItem('theme', theme);
+    });
+});
